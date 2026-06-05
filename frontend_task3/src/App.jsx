20ed488aee7ff20f422465c8b8c454a9d3acd8d8
mailbotprem-haha <mailbotprem@gmail.com>
2026-06-05 22:00:12 +0530

    Update App.jsx

--- frontend_task3/src/App.jsx
+++ frontend_task3/src/App.jsx
@@ -1,28 +1,29 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import BlogDashboard from "./pages/BlogDashboard";
 
 function App() {
   return (
     <Routes>
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-      <Route
-        path="/signup"
-        element={<Signup />}
-      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route
         path="/"
         element={
           localStorage.getItem("token") ? (
             <Dashboard />
+          ) : (
+            <Navigate to="/login" />
+          )
+        }
+      />
+      <Route
+        path="/blogs"
+        element={
+          localStorage.getItem("token") ? (
+            <BlogDashboard />
           ) : (
             <Navigate to="/login" />
           )
